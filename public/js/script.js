295af1631a29b5d8514f690ac4cb5fc947c3be99
Jack Fergusson <jfergusson@hbng.ca>
2024-08-08 16:26:05 -0400

    Initial commit.

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,34 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function dimensionDropDown() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+}
+
+let board = document.getElementById("board");
+let visibleExcess = board.getElementsByClassName("excess visible");
+let invisibleExcess = board.getElementsByClassName("excess invisible");
+
+
+function threebythree() {
+
+    // alert("three");
+
+    board.classList.add("threebythree");
+    while (visibleExcess.length > 0) {
+        visibleExcess.item(0).classList.add("invisible");
+        visibleExcess[0].classList.remove("visible");
+    }
+}
+
+function fourbyfour() {
+    // alert("four");
+    board.classList.remove("threebythree");
+    while (invisibleExcess.length > 0) {
+        invisibleExcess.item(0).classList.add("visible");
+        invisibleExcess[0].classList.remove("invisible");
+    }
+}
+
+// function hello() {
+//     alert("Hello!");
+// }
