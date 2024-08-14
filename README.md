b732e6743bfda3eac82cef28166fef0a48d180e9
Jack Fergusson <jfergusson@hbng.ca>
2024-08-13 22:07:47 -0400

    Removed multiple board sizes. Initial DB work.

---
+++ README.md
@@ -0,0 +1,16 @@
+Bingo Card Project
+
+NOTES:
+
+    - Going to have to use persistence. Likely MongoDB. This will retain all the squares in a particular sub-URL.
+    - Upon form submission, the 9 entries will be added to DB along with unique random identifier and OG ordering
+    - Each new user will get sub-sub-URL with a spot in the DB and new random BINGO ordering
+    - Socket.io to maintain realtime dashboard of alll user's BINGO boards, also updated in the DB.
+
+TO DO:
+
+    Step 1 (3x3 with sharing)
+
+    - Get 9 inputs from user + display name
+    - Create a new page with a random URL for this unique board
+    - New clicks to the link prompt a user name and get randomized board in URL/1,2,3 etc.
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
 // initialize the application
 const app = express();
@@ -11,9 +12,70 @@ app.set('views', (__dirname + '/views'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'BingoDB';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('Rooms');
+
+  // the following code examples can be pasted here...
+  const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+  console.log('Inserted documents =>', insertResult);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
 // root GET request
 app.get('/', (req, res) => {
   res.render("home");
+});
+
+let squares = [];
+let allowedRoomIDs = [];
+
+app.post('/form', (req, res) => {
+
+  squares = [];
+
+  for (var i = 1; i <= 9; i++) {
+    squares.push(req.body[i]);
+  }
+
+  allowedRoomIDs.push(req.body.roomID);
+
+  res.redirect("/rooms/" + req.body.roomID);
+});
+
+app.get('/rooms/:roomID', (req, res) => {
+  console.log(req.body);
+
+  if (allowedRoomIDs.includes(req.params.roomID)) {
+    res.render("room", { 
+      roomID: req.params.roomID,
+      squares: squares
+    });
+  }
+  else {
+    console.log("ERROR: No such room");
+    res.redirect("/error");
+  }
+}); 
+
+app.get('/error', (req, res) => {
+  res.render("error");
 });
 
 
