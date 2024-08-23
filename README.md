893c4d20d5c3e074f98e18a7b7c3d087730d81ec
Jack Fergusson <jfergusson@hbng.ca>
2024-08-23 13:32:00 -0400

    Initial socket configuration and individual users can join game

--- README.md
+++ README.md
@@ -12,7 +12,16 @@ TO DO:
 
     Step 1 (3x3 with sharing)
 
-    - Get 9 inputs from user + display name
-    - Create a new page with a random URL for this unique board
-    - New clicks to the link prompt a user name and get randomized board in URL/1,2,3 etc.
     - Auto redirect to correct room based on cookies.
+    - Create middleware to check db for user ID correctness
+    - Create the boards page to show blank versions of all boards in this room in the db with names
+    - Use sockets with rooms to have each user's boards updated on the main boards page.
+
+
+FLOW:
+
+    /: Enter home page. If cookie for roomID -> route to roomID. Else get them to fill out form for new room
+    /roomID: Purely routing. If no playerID cookie route to signup. Else route to playerID page
+    /roomID/playerID: View and make changes to your individual board. 
+    /roomID/boards: See all boards
+
--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
 const cookieParser = require("cookie-parser");
+const ejs = require("ejs");
 
 const helpers = require('./helpers');
 
 // initialize the application
 const app = express();
+const server = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 // set the view engine and licate the views folder
 app.set('view engine', 'ejs');
 app.set('views', (__dirname + '/views'));
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/BingoDB');
@@ -26,6 +30,7 @@ const playerSchema = new Schema({
   ID: String,
   DisplayName: String,
   BoardOrder: [Number],
+  BoardState: [Boolean],
   WinCondition: Boolean,
 });
 
@@ -49,13 +54,25 @@ const Player = mongoose.model(
 
 // root GET request
 app.get('/', (req, res) => {
-
   res.render("home");
+});
+
+io.on("connection", (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('room ID', (ID) => {
+    socket.join(ID);
+    console.log("A user joined room " + ID);
+    console.log(socket.rooms);
+  })
+
 });
 
 app.post('/', async (req, res, next) => {
   if (req.cookies.roomID) {
-    console.log("Oh my got it worked: " + req.cookies.roomID);
     res.redirect("/rooms/" + req.cookies.roomID);
   }
   else {
@@ -66,7 +83,7 @@ app.post('/', async (req, res, next) => {
     // Create a new 6-letter ID for this specific room
     roomInstance.ID = helpers.makeID(8);
 
-    res.cookie("roomID", roomInstance.roomID, {maxAge: 360000}, "/");
+    res.cookie("roomID", roomInstance.roomID, {maxAge: 3600000000}, "/");
 
     let events = [];
     // Get the events from the 9 squares in form
@@ -82,14 +99,13 @@ app.post('/', async (req, res, next) => {
   }
 });
 
-app.get('/rooms/:roomID', async (req, res) => {
+// Middleware for checking roomID validity in the URL
+async function checkRoomID(req, res, next) {
   let roomIDs = [];
 
   // Get the IDs of all existing rooms to see if the requested URL is valid
   await Room.find({}, 'ID').exec()
     .then(function(results) {
-      console.log(results);
-
       results.forEach((result) => roomIDs.push(result.ID));
     })
     .catch(function(err) {
@@ -99,31 +115,90 @@ app.get('/rooms/:roomID', async (req, res) => {
   );
 
   if (roomIDs.includes(req.params.roomID)) {
-    res.cookie("roomID", req.params.roomID, {maxAge: 360000}, "/")
-    // Query this room's squares from database
-    let squares = [];
-    await Room.find({ ID: req.params.roomID }, 'Events').exec()
-      .then(function(results) {
-        squares = results[0].Events;
-      }
-    );
-    console.log(squares);
-
-    // Render the room with the found roomID and squares.
-    res.render("room", { 
-      roomID: req.params.roomID,
-      squares: squares,
-    });
+    console.log("ROOM ID VERIFIED");
+    next();
   }
   else {
     console.log("ERROR: No such room: " + req.params.roomID);
     res.redirect("/error");
   }
+}
+
+// Route to the right sub-URL depending on cookie info
+app.get("/rooms/:roomID", checkRoomID, async (req, res) => {
+  if (typeof req.cookies.playerID == String) {
+    res.redirect("/rooms/" + req.params.roomID + "/" + req.cookies.playerID);
+    // res.redirect("/rooms/" + req.params.roomID + "/boards");
+  }
+  else {
+    res.redirect("/rooms/" + req.params.roomID + "/signup");
+  }
+});
+
+app.get('/rooms/:roomID/boards', checkRoomID, async (req, res) => {
+
+  // Refresh the cookie
+  res.cookie("roomID", req.params.roomID, {maxAge: 3600000000}, "/");
+
+  let squares = [];
+  // Query this room's squares from database
+  await Room.find({ ID: req.params.roomID }, 'Events').exec()
+    .then(function(results) {
+      squares = results[0].Events;
+    }
+  );
+
+  // Render the room with the found roomID and squares.
+  res.render("room", { 
+    roomID: req.params.roomID,
+    squares: squares,
+  });
 }); 
+
+app.get("/rooms/:roomID/signup", checkRoomID, (req, res) => {
+  res.render("signup", {
+    roomID: req.params.roomID,
+  });
+});
+
+// A user joins the game
+app.post("/rooms/:roomID/signup", async (req, res) => {
+  console.log(req.body.displayName);
+  console.log(req.params.roomID);
+
+  const playerInstance = new Player();
+
+  playerInstance.ID = helpers.makeID(6);
+  playerInstance.DisplayName = req.body.displayName;
+  playerInstance.BoardOrder = helpers.shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+  playerInstance.WinCondition = false;
+  playerInstance.BoardState = [false, false, false, false, false, false, false, false, false];
+
+  // playerInstance.save();
+
+  // Add the new player to the db using Mongoose syntax, not MongoDB.
+  const room = await Room.findOne({ ID: req.params.roomID });
+
+  room.Players.push(playerInstance);
+  room.save();
+
+  // Save a cookie to remember this player
+  res.cookie("playerID", playerInstance.ID, {maxAge: 3600000000}, "/");
+
+  // Redirect them to their personal view.
+  res.redirect("/rooms/" + req.params.roomID + "/" + playerInstance.ID);
+});
+
+app.get("/rooms/:roomID/:playerID", checkRoomID, async (req, res) => {
+  console.log("Made it to the player page");
+  console.log(req.cookies.playerID);
+  res.render("player");
+});
 
 app.get('/error', (req, res) => {
   res.render("error");
 });
 
-
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+server.listen(3000, () => {
+  console.log('listening on port 3000')
+});
